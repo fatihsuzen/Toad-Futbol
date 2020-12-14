7ac45eda80f3d635944e0fadd06ae240e56e3ad8
fatihsuzen <fatihsuzenbjk@gmail.com>
2020-12-14 13:27:02 +0300

    fans complete

---
+++ Assets/Fans.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fans : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("RandomAnim", Random.Range(0, 3), Random.Range(5,15));
+    }
+    
+    void RandomAnim()
+    {
+        GetComponent<Animator>().SetInteger("Status",Random.Range(0,5));
+    }
+}
